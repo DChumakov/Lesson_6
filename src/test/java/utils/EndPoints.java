a6d0b3fb03d077361dc65c34d25dbf0d027c54df
Dmitry Chumakov <lordtennis@mail.ru>
2022-01-31 19:15:10 +0300

    Allur 31/01/2022

--- src/test/java/utils/EndPoints.java
+++
@@ -1,6 +0,0 @@
-package utils;
-
-public class EndPoints {
-    public static final String GET_ALL_USERS = "index.php?/api/v2/get_users";
-    public static final String GET_USER = "index.php?/api/v2/get_user/{id}";
-}
